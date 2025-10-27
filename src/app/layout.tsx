d199fef93cae214264a711c9fcb578b58f50d75b
DeepThought EduTech Ventures <105041339+deepthoughtEdu@users.noreply.github.com>
2025-10-27 19:14:23 +0530

    Microsoft -clarity fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,17 +27,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-      <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "tpjkvkh7e5");
-          `}
-        </Script>
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -46,15 +35,15 @@ export default function RootLayout({
         </Header>
         {children}
         {/* Microsoft Clarity Script */}
-        {/* <Script id="microsoft-clarity" strategy="afterInteractive">
+         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "tpjkvkh7e5");
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tke8yudc2t");
           `}
-        </Script> */}
+        </Script> 
       </body>
     </html>
   );
